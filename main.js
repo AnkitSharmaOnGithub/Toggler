feab73fa382f484c2ba220c6f3dbe0b8e4e190b1
Ankit Kumar Sharma <53289186+AnkitSharmaOnGithub@users.noreply.github.com>
2019-08-08 16:32:49 +0530

    Updated Index.html and main.js
    Added Checked By Default Option

--- main.js
+++ main.js
@@ -1,89 +1,115 @@
 //Pure Js Library By Ankit Sharma
 //Warning :- Do not copy until you have authority to do so. (:D
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("DOMContentLoaded", function() {
+  //CheckBox Options
+  let is_checked = " ";
 
-//Slider Options
-let slider_position  = "absolute";
-let slider_height = "34px";
-let slider_width = "78px";
-let slider_bg = "#ccc";
-let slider_on_bg = "#2196f3";
-let slider_shadow = "0 0 1px #2196f3";
-let cursor = "pointer";
+  //Slider Options
+  let slider_position = "absolute";
+  let slider_height = "34px";
+  let slider_width = "78px";
+  let slider_bg = "#ccc";
+  let slider_on_bg = "#2196f3";
+  let slider_shadow = "0 0 1px #2196f3";
+  let cursor = "pointer";
 
-//Trigger Options
-let trigger_position = "absolute";
-let trigger_height = "24px";
-let trigger_width = "24px";
-let trigger_left = "4px";
-let trigger_bottom = "4px";
-let trigger_margin = "4px";
-let trigger_bg = "#ffffff";
-let trigger_trasition = "translateX(44px)";
-let transitionDuration = "1s";
-let trigger_deactivated_transition = "translateX(0px)";
+  //Trigger Options
+  let trigger_position = "absolute";
+  let trigger_height = "24px";
+  let trigger_width = "24px";
+  let trigger_left = "4px";
+  let trigger_bottom = "4px";
+  let trigger_margin = "4px";
+  let trigger_bg = "#ffffff";
+  let trigger_trasition = "translateX(44px)";
+  let transitionDuration = "1s";
+  let trigger_deactivated_transition = "translateX(0px)";
 
-var switches = {};
-var toggles = {
-  "toggle-default": {
-   
-  },
-  "toggle-small":{
-    slider_width : "60px",
-    slider_height : "25px",
-    trigger_width : "17px",
-    trigger_height : "17px",
-    trigger_trasition : "translateX(36px)",
-  }
-};
-
-var Switch = function(el, options) {
-  if (el !== undefined || options !== undefined) {
-    this._init(el, options);
-  }
-};
-
-Switch.prototype._init = function(el, options) {
-  //Constant Variables
-
-  let defaultOptions = {
-
-    sliderOptions : {
-        position : slider_position,
-        cursor : cursor,
-        backgroundColor : slider_bg,
-        width :  options.slider_width == undefined ? slider_width :options.slider_width,
-        height : options.slider_height == undefined ? slider_height :options.slider_height,
+  var switches = {};
+  var toggles = {
+    "toggle-default": {},
+    "toggle-small": {
+      slider_width: "60px",
+      slider_height: "25px",
+      trigger_width: "17px",
+      trigger_height: "17px",
+      trigger_trasition: "translateX(36px)"
     },
-
-    TriggerOptions : {
-        trigger_width : options.trigger_width == undefined ? trigger_width :options.trigger_width,
-        trigger_height : options.trigger_height == undefined ? trigger_height :options.trigger_height,
-        boxShadow : slider_shadow,
-        trigger_trasition : options.trigger_trasition == undefined ? trigger_trasition :options.trigger_trasition,
-        transitionDuration : transitionDuration,
-    },
-
-    //Slider
-    
-
+    checked: {
+      is_checked: "checked"
+    }
   };
-  console.log(defaultOptions.sliderOptions.width);
-  el.style.opacity = 0;
 
-  el.addEventListener('change', function(){
-      isChecked(this,defaultOptions.TriggerOptions);
-  });
+  var Switch = function(el, options) {
+    if (el !== undefined || options !== undefined) {
+      this._init(el, options);
+    }
+  };
 
-  setSlider(el,defaultOptions.sliderOptions);
-  setTrigger(el,defaultOptions.TriggerOptions);
-};
+  Switch.prototype._init = function(el, options) {
+    //Constant Variables
+
+    let defaultOptions = {
+      sliderOptions: {
+        position: slider_position,
+        cursor: cursor,
+        backgroundColor: slider_bg,
+        width:
+          options.slider_width == undefined
+            ? slider_width
+            : options.slider_width,
+        height:
+          options.slider_height == undefined
+            ? slider_height
+            : options.slider_height
+      },
+
+      TriggerOptions: {
+        trigger_width:
+          options.trigger_width == undefined
+            ? trigger_width
+            : options.trigger_width,
+        trigger_height:
+          options.trigger_height == undefined
+            ? trigger_height
+            : options.trigger_height,
+        boxShadow: slider_shadow,
+        trigger_trasition:
+          options.trigger_trasition == undefined
+            ? trigger_trasition
+            : options.trigger_trasition,
+        transitionDuration: transitionDuration
+      }
+
+      //Slider
+    };
+
+    el.style.opacity = 0;
+
   
+    el.addEventListener("change", function() {
+      isChecked(this, defaultOptions.TriggerOptions);
+    });
 
+    
+    setSlider(el, defaultOptions.sliderOptions);
+    setTrigger(el, defaultOptions.TriggerOptions);
+    if (options.is_checked != undefined) {
+      setCheckbox(el, options);
+      isChecked(el, defaultOptions.TriggerOptions);
+    }
+  };
 
-//   SetSlider Function
-  function setSlider(element, sliderOptions){
+  //Set CheckBox options
+  function setCheckbox(element, Options) {
+    console.log(Options);
+    is_checked = Options.is_checked == undefined ? " " : "checked";
+    element.setAttribute("checked", is_checked);
+  }
+
+  //   SetSlider Function
+  function setSlider(element, sliderOptions) {
     var slider = element.nextElementSibling.style;
     slider.position = sliderOptions.position;
     slider.cursor = sliderOptions.cursor;
@@ -93,7 +119,7 @@ Switch.prototype._init = function(el, options) {
   }
 
   //SetTrigger Function
-  function setTrigger(element, TriggerOptions){
+  function setTrigger(element, TriggerOptions) {
     var Trigger = element.parentElement.lastElementChild.style;
     Trigger.position = trigger_position;
     Trigger.cursor = cursor;
@@ -105,31 +131,28 @@ Switch.prototype._init = function(el, options) {
 
   //SetCheckedProperties Function
 
-  function isChecked(element,options){
+  function isChecked(element, options) {
     console.log(options);
     var Trigger = element.parentElement.lastElementChild.style;
 
-    if(element.checked){
-        if(element == document.activeElement){
-            element.nextElementSibling.style.boxShadow  = slider_shadow;
-            element.nextElementSibling.style.backgroundColor = slider_on_bg;
-            Trigger.transform = options.trigger_trasition;
-            Trigger.transitionDuration = transitionDuration;
-        }
-    }
-    else{
-        if(this != document.activeElement){
-            element.nextElementSibling.style.boxShadow  = " ";
-          
-          element.nextElementSibling.style.backgroundColor = slider_bg;
-          Trigger.transform = trigger_deactivated_transition;
-        }
+    if (element.checked) {
+      // if (element == document.activeElement) {
+        element.nextElementSibling.style.boxShadow = slider_shadow;
+        element.nextElementSibling.style.backgroundColor = slider_on_bg;
+        Trigger.transform = options.trigger_trasition;
+        Trigger.transitionDuration = transitionDuration;
+      // }
+    } else {
+      // if (this != document.activeElement) {
+        element.nextElementSibling.style.boxShadow = null;
+
+        element.nextElementSibling.style.backgroundColor = slider_bg;
+        Trigger.transform = trigger_deactivated_transition;
+      // }
     }
   }
 
-Object.keys(toggles).forEach(function(key) {
-  switches[key] = new Switch(document.querySelector("." + key), toggles[key]);
-});
-
-
+  Object.keys(toggles).forEach(function(key) {
+    switches[key] = new Switch(document.querySelector("." + key), toggles[key]);
+  });
 });
